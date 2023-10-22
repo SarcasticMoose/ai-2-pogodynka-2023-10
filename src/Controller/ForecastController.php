83683596026c76a1482435df2ed68650b9bd811f
Jakub Tokarczyk <kubat260@gmail.com>
2023-10-22 22:52:04 +0200

    Lab-c

---
+++ src/Controller/ForecastController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\City;
+use App\Repository\CityRepository;
+use App\Repository\ForecastRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForecastController extends AbstractController
+{
+
+    #[Route('/forecast/{id}', name: 'app_forecast', requirements: ['id' => '\d+'])]
+    public function index(): Response
+    {
+        return $this->render('forecast/city.html.twig', [
+            'controller_name' => 'ForecastController',
+        ]);
+    }
+
+    #[Route('/wheather/{cityName}/{countryCode}',defaults: ['countryCode' => 'PL'])]
+    public function GetForecast(
+        ForecastRepository $forecastRepository,
+        ?string $countryCode,
+        #[MapEntity(expr: 'repository.findByName(cityName,countryCode)')]
+        City $city) : Response
+    {
+        $forecast = $forecastRepository->findByLocalization($city);
+
+        return $this->render("forecast/city.html.twig",[
+            'city' => $city,
+            'forecast' => $forecast
+        ]);
+    }
+}
